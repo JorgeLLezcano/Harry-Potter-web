e379226ac6703ef26d66e2c2c1945e328cdbcc98
jorge Lezcano <jorgeluislezcano1988@gmail.com>
2024-07-02 16:29:54 -0300

    nuevos estilos a character

--- Harry-Potter-WebSite/src/Character.jsx
+++ Harry-Potter-WebSite/src/Character.jsx
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import './hojaDeEstilo/character.css'
 
 export default function Character({ characters }) {
   const { id } = useParams();
@@ -9,12 +10,38 @@ export default function Character({ characters }) {
   }
 
   return (
-    <div>
+    <div className="tarjeta-character">
+      <div>
       <h1>{character.name}</h1>
       <img src={character.image} alt={character.name} />
-      <p>House: {character.house}</p>
+      </div>
+      <div className="bandera"
+      style={
+        {   
+          backgroundColor: character.house === 'Gryffindor' ?
+          'red' : character.house === 'Hufflepuff' ?
+          'yellow' : character.house === 'Ravenclaw' ?
+            'blue' : 'green'
+          }
+          }>
+      <p
+      style={
+        {   
+          backgroundColor: character.house === 'Gryffindor' ?
+          'yellow' : character.house === 'Hufflepuff' ?
+          'black' : character.house === 'Ravenclaw' ?
+            'brown' : 'grey',
+
+            color: character.house==='Hufflepuf'? "black":"white"
+          }
+          }
+      >House: {character.house}</p>
+      </div>
+      <div className="meta-data">
       <p>Alternate Names: {character.alternate_names.join(', ')}</p>
       <p>Ancestry: {character.ancestry}</p>
+      </div>
+     <Link to={'/'}> Back </Link>
     </div>
   );
 }
